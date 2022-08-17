a1cc750403018c657528259133ac4e70c947e64c
Mikhail Novikov <10743009+notiv-nt@users.noreply.github.com>
2022-08-17 17:23:36 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: 'tvl-watcher',
       script: './src/main.js',
       instances: 1,
+      exec_mode: 'fork',
       env_production: {
         NODE_ENV: 'production',
       },
